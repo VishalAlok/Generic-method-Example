2d685c260c358c9b6d0a5ad91d1d9dca3a1543f7
vishal.Alok <you@example.com>
2021-10-14 06:18:07 +0530

    New changes

--- Generic/Program.cs
+++ Generic/Program.cs
@@ -14,10 +14,14 @@ namespace Generic
 
         static void Main(string[] args)
         {
-            bool result1= EqualCheck(10,10);
-            bool result2 = EqualCheck("Vishal", "Vishal");
-            bool result3 = EqualCheck('V', 'V');
+            bool result1 = EqualCheck(10, 10);
+            bool result2 = EqualCheck(10, 10);
+            bool result3 = EqualCheck("Vishal", "Vishal");
+            bool result4 = EqualCheck<string>("Vishal", "Vishal");
+            bool result5 = EqualCheck('V', 'V');
+            bool result6 = EqualCheck<char>('V', 'V');
             Console.WriteLine(result1.ToString() +  result2.ToString() +  result3.ToString());
+            Console.WriteLine(result4.ToString() + result5.ToString() + result6.ToString());
         }
     }
 }
